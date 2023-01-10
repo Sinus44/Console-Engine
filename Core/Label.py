6f32cca7f31cd141683c4f10c3fc70b3f382e3c1
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2023-01-10 10:08:33 +0300

    Delete Core directory

--- Core/Label.py
+++
@@ -1,8 +0,0 @@
-from Core.Element import Element
-
-class Label(Element):
-    """GUI - элемент текст"""
-    
-    def draw(self):
-        if self.visible:
-            self.screen.text(self.text, self.x, self.y, wordPrefix=self.style["background"] + self.style["text"])
